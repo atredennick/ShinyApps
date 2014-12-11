0d355d040db1c3790930ee3d7e23340dad3fa00c
Andrew Tredennick <atredenn@gmail.com>
2014-12-11 14:04:58 -0700

    updated ui
    renamed the full ui for now so that only the working bits show up in
    ui.R — so just the storage effect for now

--- Chesson_incomplete/ChessonApp/ui.R
+++ Chesson_incomplete/ChessonApp/ui.R
@@ -6,214 +6,70 @@
 #
 library(shiny)
 library(markdown)
+library(ggplot2)
 
 shinyUI(navbarPage("Chesson Tutorial",
-                   tabPanel("Simple Population Growth",
-                            # Sidebar with a slider input for number of bins
-                            sidebarLayout(
-                              sidebarPanel(
-                                helpText("First we imagine a species with a growth rate:"),
-                                sliderInput("Lam",
-                                            label=div(HTML("Choose &lambda;:")),
-                                            min = .95,
-                                            max = 1.05,
-                                            value = 1.01),
-                                                              
-                                helpText("The slope of the log(N) line, 
-               especially at t<30, gives the approximate 
-               long-term low density growth rate"),
-                                br(),
-                                
-                                radioButtons("sd", "Is there stochastic variation in your population?:",
-                                             c("No" = "no",
-                                               "Yes"= "yes")),
-                                br(),
-                                
-                                sliderInput("sdLam",
-                                            label=c("The Std. Dev of Lambda"),
-                                            min = .01,
-                                            max = .05,
-                                            value = .01)
-                                
-                                ),
-                              
-                              # Show a plot of the generated distribution
-                              mainPanel(
-                                plotOutput("popGrow"),
-                                br(),
-                                helpText("Below is the equation of the orange line, which is a linear model for timesteps 1-30"),
-                                verbatimTextOutput("summary")
-                              )
-                            )),
-                  
-                   tabPanel("Lotka-Volterra Competition",
-                    sidebarLayout(
-                     sidebarPanel(
-                       sliderInput("Sp1R",
-                                   label="Species 1 per capita rate of increase",
-                                   min = 0,
-                                   max = 1.2,
-                                   value = .3, step=.1),
-                       sliderInput("Sp2R",
-                                   label="Species 2 per capita rate of increase",
-                                   min = 0,
-                                   max = 1.2,
-                                   value = .4, step=.1),
-                       br(),
-                       radioButtons("setK", "Is there a Carrying Capacity (K)?",
-                                    c("No" = "no",
-                                      "Yes"= "yes")),
-                       
-                       br(),
-                       radioButtons("incAlpha1", "Do these species negatively affect themselves (density dependence)?:",
-                                    c("No" = "no",
-                                      "Yes"= "yes")),
-                       
-                       sliderInput("a11",
-                                   label=c("The effect of species 1 on species 1"),
-                                   min = .1,
-                                   max = 1.2,
-                                   value = .2, step=.1),
-                       
-                       sliderInput("a22",
-                                   label=c("The effect of species 2 on species 2"),
-                                   min = .1,
-                                   max =1.2,
-                                   value = .8, step=.1),
-                       br(),
-                       
-                       radioButtons("incAlpha2", "Do these species interact?:",
-                                    c("No" = "no",
-                                      "Yes"= "yes")),
-          
-                       sliderInput("a21",
-                                   label=c("The effect of species 2 on species 1"),
-                                   min = .1,
-                                   max = 1.2,
-                                   value = .2, step=0.1),
-                       
-                       sliderInput("a12",
-                                   label=c("The effect of species 1 on species 2"),
-                                   min = .1,
-                                   max = 1.2,
-                                   value = 1.1, step=0.1)
-                                              
-                       
-                     ),
-                                        
-                     
-                     # Show a plot of the generated distribution
-                     mainPanel(
-                       helpText("Orange line is Species 1, Blue is Species 2"),
-                       plotOutput("LVComp"),
-                       helpText("Species can exist when a species 
-                                is more limited by other members of 
-                                its own species, than by members of 
-                                other species; aii>aij or aij/ajj<1"))                                            )
-                   ),
-                   
-                   tabPanel("War Game Relative Nonlinearity",
-                            sidebarLayout(
-                              sidebarPanel(
-                                sliderInput("CSp1R",
-                                            label="Species 1 per capita rate of increase",
-                                            min = 0,
-                                            max = 1.2,
-                                            value = .3, step=.1),
-                                sliderInput("CSp2R",
-                                            label="Species 2 per capita rate of increase",
-                                            min = 0,
-                                            max = 1.2,
-                                            value = .4, step=.1),
-                                br(),
-                                radioButtons("CsetK", "Is there a Carrying Capacity (K)?",
-                                             c("No" = "no",
-                                               "Yes"= "yes")),
-                                
-                                br(),
-                                radioButtons("CincAlpha1", "Do these species negatively affect themselves?:",
-                                             c("No" = "no",
-                                               "Yes"= "yes")),
-                                
-                                sliderInput("Ca11",
-                                            label=c("The effect of species 1 on species 1"),
-                                            min = .1,
-                                            max = 1.2,
-                                            value = .2, step=.1),
-                                
-                                sliderInput("Ca22",
-                                            label=c("The effect of species 2 on species 2"),
-                                            min = .1,
-                                            max =1.2,
-                                            value = .8, step=.1),
-                                br(),
-                                
-                                radioButtons("CincAlpha2", "Do these species interact?:",
-                                             c("No" = "no",
-                                               "Yes"= "yes")),
-                                
-                                sliderInput("Ca21",
-                                            label=c("The effect of species 2 on species 1"),
-                                            min = .1,
-                                            max = 1.2,
-                                            value = .2, step=0.1),
-                                
-                                sliderInput("Ca12",
-                                            label=c("The effect of species 1 on species 2"),
-                                            min = .1,
-                                            max = 1.2,
-                                            value = 1, step=0.1),
-                                
-                                br(),
-                                h4("Check your nonlinearity"),
-                              
-                                radioButtons("CincNL", "Do these species exhibit nonlinearity?:",
-                                             c("No" = "no",
-                                               "Yes"= "yes")),
-                                
-                                br(),
-                                helpText("R<-log(x)/(a+b*log(x))"),
-                                
-                                
-                                sliderInput("Sp1a",
-                                            label="Species 1 a",
-                                            min = .05,
-                                            max = .95,
-                                            value = .5, step=.05),
-                                
-                                sliderInput("Sp1b",
-                                            label="Species 1 b",
-                                            min = .05,
-                                            max = .95,
-                                            value = .7, step=.05),
-                                
-                                helpText("**Lower this:"),
-                                sliderInput("Sp2a",
-                                            label="Species 2 a",
-                                            min = .05,
-                                            max = .95,
-                                            value = .5, step=.05),
-                                
-                                sliderInput("Sp2b",
-                                            label="Species 2 b",
-                                            min = .05,
-                                            max = .95,
-                                            value = .9, step=.05)
-                                
-                              ),
-                              
-                              
-                              # Show a plot of the generated distribution
-                              mainPanel(
-                                helpText("Orange line is Species 1, Blue is Species 2"),
-                                plotOutput("Chess"),
-                                helpText("Int he L-V model, species can 
-                                coexist when a species 
-                                is more limited by other members of 
-                                its own species, than by members of 
-                                other species; aii>aij or aij/ajj<1"))                                            )
-                   ),
-
                    tabPanel("The Storage Effect",
-                            h2("Nope."))
-                   ))
+                            sidebarLayout(
+                              sidebarPanel(
+                                sliderInput("timeSim",
+                                            label="Length of simulation (years or generations)",
+                                            min = 10,
+                                            max = 1000,
+                                            value = 100, step=1),
+                                sliderInput("s1",
+                                            label="Species 1 survival of seeds in seedbank",
+                                            min = 0,
+                                            max = 1,
+                                            value = 0.5, step=0.1),
+                                sliderInput("s2",
+                                            label="Species 2 survival of seeds in seedbank",
+                                            min = 0,
+                                            max = 1,
+                                            value = 0.5, step=0.1),
+                                sliderInput("lambda1",
+                                            label="Species 1 per capita fecundity",
+                                            min = 10,
+                                            max = 100,
+                                            value = 50, step=5),
+                                sliderInput("lambda2",
+                                            label="Species 2 per capita fecundity",
+                                            min = 10,
+                                            max = 100,
+                                            value = 50, step=5),
+                                sliderInput("alpha11",
+                                            label="Effect of species 1 on itself (intraspecific competition)",
+                                            min = 0,
+                                            max = 4,
+                                            value = 1, step=0.1),
+                                sliderInput("alpha12",
+                                            label="Effect of species 2 on species 1 (interspecific competition)",
+                                            min = 0,
+                                            max = 4,
+                                            value = 1, step=0.1),
+                                sliderInput("alpha22",
+                                            label="Effect of species 2 on itself (intraspecific competition)",
+                                            min = 0,
+                                            max = 4,
+                                            value = 1, step=0.1),
+                                sliderInput("alpha21",
+                                            label="Effect of species 1 on species 2 (interspecific competition)",
+                                            min = 0,
+                                            max = 4,
+                                            value = 1, step=0.1),
+                                sliderInput("sigE",
+                                            label="Environmental variation",
+                                            min = 0,
+                                            max = 10,
+                                            value = 1, step=0.1),
+                                sliderInput("rho",
+                                            label="Correlation between species' germination rates",
+                                            min = -1,
+                                            max = 1,
+                                            value = 0, step=0.1)),
+                              # Show a plot of the generated distribution
+                              mainPanel(
+                                helpText("Grey line is Species 1 (A), orange is Species 2 (B)"),
+                                plotOutput("StorageEffect"),
+                                helpText("Here is some text."))
+                   ))))
--- Chesson_incomplete/RunApp_Chesson.R
+++ Chesson_incomplete/RunApp_Chesson.R
@@ -1,7 +1,8 @@
 #Set your working drive here so that the folder of your app is in the directory
 #Here are my funny paths:
-setwd("C:/Users/A02077138/Documents/GitHub/ShinyApps/Chesson")
+# setwd("C:/Users/A02077138/Documents/GitHub/ShinyApps/Chesson")
 #setwd("C:/Users/Britta/Dropbox/A.weekender/GitHub/Sandbox/ShinyApps/Chesson")
+setwd("~/Repos/ShinyApps/Chesson_incomplete")
 
 #If you don't have devtools or shinyapps:
 #install.packages('devtools')
@@ -10,6 +11,7 @@ setwd("C:/Users/A02077138/Documents/GitHub/ShinyApps/Chesson")
 library(shiny)
 library(devtools)
 library(shinyapps)
+library(ggplot2)
 
 #Run app locally
 runApp("ChessonApp")
